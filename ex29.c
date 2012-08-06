fdffd98bf2444b9c339798ef67099ef43636e397
Frank <francisco.licea@gmail.com>
2012-08-06 10:04:41 -0500

    Add ex29

---
+++ ex29.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "debug.h"
+#include <dlfcn.h>
+
+typedef int (*lib_func)(const char *data);
+
+int main(int argc, char *argv[])
+{
+	int rc = 0;
+	check(argc == 4, "USAGE: ex29 libex29.so function data");
+
+	char *lib_file = argv[1];
+	char *func_to_run = argv[2];
+	char *data = argv[3];
+	
+	void *lib = dlopen(lib_file, RTLD_NOW);
+	check(lib != NULL, "Failed to open the library %s: %s", lib_file, dlerror());
+
+	lib_func func = dlsym(lib, func_to_run);
+	check(func != NULL, "Could not find %s function in the library %s: %s", func_to_run, lib_file, dlerror());
+
+	rc = func(data);
+	check(0 == rc, "Function %s return %d for data: %s", func_to_run, rc, data);
+
+	rc = dlclose(lib);
+	check(0 == rc, "Failed to close %s", lib_file);
+
+	return 0;
+
+error:
+
+	return 1;
+}
